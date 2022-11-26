5b253ff2b808d87570d3b3bd4ac8728b3e821335
zankoo malaiy <102598239+zankoocode@users.noreply.github.com>
2022-11-26 13:16:10 +0330

    design contract

--- contracts/contracts/DAO.sol
+++ contracts/contracts/DAO.sol
@@ -11,7 +11,7 @@ interface INFTMarketPlace {
 }   
 
 interface IZCDtoken {
-    function balanceOf(address owner) public view returns (uint256);
+    function balanceOf(address owner) external view returns (uint256);
 }
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -31,7 +31,7 @@ contract DAO is Ownable {
     // executed - whether or not this proposal has been executed yet. Cannot be executed before the deadline has been exceeded.
         bool executed;
     // voters - a mapping of CryptoDevsNFT tokenIDs to booleans indicating whether that NFT has already been used to cast a vote or not
-        mapping(uint256 => bool) voters;
+        mapping(address => bool) voters;
     }
 
     // Create a mapping of ID to Proposal
@@ -48,11 +48,69 @@ contract DAO is Ownable {
     }
 
     modifier onlyTokenHolder {
-        require(NFTMarketPlace.balanceOf(msg.sender) > 0, "not a DAO member");
-
+        require(ZCDToken.balanceOf(msg.sender) > 0, "not a DAO member");
+        _;
     }
 
     function createProposal (uint256 tokenId) external onlyTokenHolder returns (uint256) {
-        require(NFTMarketPlace.available(tokenId))
+        require(NFTMarketPlace.available(tokenId), "NFT is not available");
+        Proposal storage proposal = proposals[numProposals];
+        proposal.nftTokenId = tokenId;
+        proposal.deadline = block.timestamp + 24 hours;
+        numProposals++;
+        return numProposals - 1;
     } 
+
+    modifier onlyActiveProposals (uint256 proposalIndex){
+        require(proposals[proposalIndex].deadline > block.timestamp, "deadline exeeded");
+        _;
+    }
+
+    enum Vote {
+        Yes, 
+        No
+    }
+
+    function voteOnProposal (uint256 proposalIndex, Vote vote) external onlyTokenHolder onlyActiveProposals (proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+        
+        uint256 numOfVotes = 0;
+        if (proposal.voters[msg.sender] == false) {
+            numProposals++;
+            proposal.voters[msg.sender] = true;
+        }
+        require (numOfVotes > 0, "you have already voted");
+
+        if (vote == Vote.Yes) {
+            proposal.yesVotes += numOfVotes;
+        } else {
+            proposal.noVotes += numOfVotes;
+        }
+    } 
+
+    modifier inActiveProposalsOnly (uint256 proposalIndex) {
+        require(proposals[proposalIndex].deadline <= block.timestamp, "deadline is not exeeded");
+        require(proposals[proposalIndex].executed == false, "proposal have already been exected");
+        _;
+    } 
+
+    function execute (uint256 proposalIndex) external onlyTokenHolder inActiveProposalsOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        if (proposal.yesVotes > proposal.noVotes) {
+            uint256 NFTPrice = NFTMarketPlace.getNFTPrice();
+            require(address(this).balance >= NFTPrice, "contract doesnt have enough funds");
+            NFTMarketPlace.purchase{value: NFTPrice}(proposal.nftTokenId);
+
+        }
+        proposal.executed = true;
+    }
+
+    function withdrawEther() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,25 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
+const ethers = require("ethers");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  
+  
+  const NFTMarketPlace = await hre.ethers.getContractFactory("NFTMarketPlace");
+  const nftmarketplace = await NFTMarketPlace.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await nftmarketplace.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    ` NFT Market Place deployed to ${nftmarketplace.address}`
+  );
+
+  const DAO = await hre.ethers.getContractFactory("DAO");
+  const dao = await DAO.deploy(
+    nftmarketplace.address,
+    "0x7f0Ed920Ed6b5bd1fBFd5Ab6b46DE777997468A5"
+  );
+
+  await dao.deployed();
+
+  console.log(
+    ` DAO deployed to ${dao.address}`
   );
 }
 
