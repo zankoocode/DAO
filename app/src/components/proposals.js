6adf52b2abc61e4a9ffcd74c6c46f2d5f018d86d
zankoo malaiy <102598239+zankoocode@users.noreply.github.com>
2022-11-27 22:58:15 +0330

    design layout

--- app/src/components/proposals.js
+++ app/src/components/proposals.js
@@ -1,7 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
 import './proposals.css';
+import { useAccount, useBalance, useContract, useContractRead, useContractWrite} from 'wagmi'
+import { ZCD_ABI, 
+         DAO_ABI,
+         DAO_CONTRACT_ADDRESS,
+         ZCD_CONTRACT_ADDRESS} from '../constants/index.js';
 
 function ProposalsTab () {
+
+    const [DAOEtherBalance, setDAOEtherBalance] = useState(0);
+
+    const [numOfProposals, setNumOfProposals] = useState(0);
+
+    const [proposals, setProposals] = useState([]);
+
+    const [ZCDBalance, setZCDBalance] = useState(0);
+
+    const [idOfProposal, setIdOfProposal] = useState(0);
+
+    const [proposalIndex, setProposalIndex] = useState("");
+
+    const [proposalToExecuteId, setProposalToExecuteToId] = useState("");
+
+    const [NFTTokenId, setNFTTokenId] = useState("");
+
+    const [vote, setVote] = useState("");
+
+    const account = useAccount();
+
+    const { refetch: getDAOEtherBalance } = useBalance({
+        address: DAO_CONTRACT_ADDRESS,
+        onSuccess(data) {
+            setDAOEtherBalance(data);
+           
+        },
+        onError(data) {
+            console.log(data)
+        }
+    })
+
+    const { refetch: getNumOfProposals } = useContractRead({
+        address: DAO_CONTRACT_ADDRESS,
+        abi: DAO_ABI,
+        functionName: 'numProposals',
+        onSuccess(data) {
+            setNumOfProposals(data);
+            
+        },
+        onError(data) {
+            console.log(data)
+        }
+    })
+
+    const { refetch: getZCDBalanceUser } = useContractRead({
+        address: ZCD_CONTRACT_ADDRESS,
+        abi: ZCD_ABI,
+        functionName: 'balanceOf',
+        args: [account.address],
+        onSuccess(data){
+            setZCDBalance(data);
+            
+        },
+        onError(data) {
+            console.log(data)
+        }
+    })
+
+    const { write: createProposal } = useContractWrite({
+        address: DAO_CONTRACT_ADDRESS,
+        abi: DAO_ABI,
+        functionName: 'createProposal',
+        args: [NFTTokenId],
+        onSettled(data) {
+             getNumOfProposals();
+        },
+        onError(data) {
+            console.log(data)
+        }
+    })
+
+    const {refetch: getProposalById } = useContractRead({
+        address: DAO_CONTRACT_ADDRESS,
+        abi: DAO_ABI,
+        functionName: 'proposals',
+        args: [idOfProposal],
+        onSuccess(data) {
+            const parse = {
+                proposalId: idOfProposal,
+                NFTTokenId: data.nftTokenId,
+                deadline: data.deadline,
+                yesVotes: data.yesVotes,
+                noVotes: data.noVotes,
+                executed: data.executed
+            }
+            return parse
+        },
+        onError(data) {
+            console.log(data)
+        }
+    })
+
+    const getAllProposals = async () => {
+        try {
+            const proposals = [];
+            while(idOfProposal < numOfProposals) {
+               const proposal = await getProposalById();
+                proposals.push(proposal);
+                idOfProposal++;
+            }
+            await setProposals(proposals);
+            return proposals
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const { write: voteOnProposal } = useContractWrite({
+        address: DAO_CONTRACT_ADDRESS,
+        abi: DAO_ABI,
+        functionName: 'voteOnProposal',
+        args: [proposalIndex, vote],
+        onSuccess(data){
+            getAllProposals();
+        },
+        onError(data) {
+            console.log(data)
+        }
+    })
+
+    const { write: executeProposal } = useContractWrite({
+        address: DAO_CONTRACT_ADDRESS,
+        abi: DAO_ABI,
+        functionName: 'execute',
+        args: [proposalToExecuteId],
+
+    })
+
+    const handleOnClickCreate = () => {
+        return (
+            <body>
+            <div className="create-page">
+                <input className="create-proposal-id" onChange={(e)=>{  setNFTTokenId(e.target.value)}} placeholder="NFT Token Id To be Proposed"/>
+                <button className="propose-btn" >
+                    Propose
+                </button>
+            </div>
+            </body>
+        )
+    }
     return (
         <div className="propaslsTab">
 
@@ -44,6 +190,11 @@ function ProposalsTab () {
                 </div>
             </div>
 
+        <div className="create-div">
+        <button className="create-btn" onClick={handleOnClickCreate}>
+            Create
+        </button>
+        </div>
             
 
         </div>
---
+++ app/src/constants/index.js
@@ -0,0 +1,469 @@
+export const DAO_CONTRACT_ADDRESS = "0x34Bc5D7C0197647F979BD0d64059acabCca55afc";
+export const ZCD_CONTRACT_ADDRESS = "0x7f0Ed920Ed6b5bd1fBFd5Ab6b46DE777997468A5";
+
+export const DAO_ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_NFTMarketPlace",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "_ZCDToken",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "fallback"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "createProposal",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "proposalIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "execute",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numProposals",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "proposals",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "nftTokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "deadline",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "yesVotes",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "noVotes",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bool",
+          "name": "executed",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "proposalIndex",
+          "type": "uint256"
+        },
+        {
+          "internalType": "enum DAO.Vote",
+          "name": "vote",
+          "type": "uint8"
+        }
+      ],
+      "name": "voteOnProposal",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdrawEther",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "receive"
+    }
+  ];
+
+  
+export const ZCD_ABI = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "subtractedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "decreaseAllowance",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "addedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "increaseAllowance",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
